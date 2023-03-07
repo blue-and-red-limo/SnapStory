760f42c0acd23f962cfb741d0ad0154adbafc9b7
eunbicho <puppy101976@naver.com>
2023-03-07 17:43:43 +0900

    Fix: delete frontend project

--- frontend/snapstory/android/app/src/main/kotlin/com/ssafy/snapstory/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ssafy.snapstory
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend/snapstory/lib/constants/routes.dart
+++
@@ -1,6 +0,0 @@
-// const rootRoute = '/';
-const loginRoute = '/login/';
-const registerRoute = '/register/';
-const mainRoute = '/main/';
-const verifyEmailRoute = '/verify-email/';
-const newNoteRoute = '/notes/new-note/';
