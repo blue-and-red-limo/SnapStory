b88fbddbd6cb526e2d326c3e5efaa25b0c9fa4e5
TEQ <thomazkwon@gmail.com>
2023-03-13 16:43:14 +0900

    Feat: fix snapshot in arviewandroid

--- frontend/snapstory/lib/views/home/find_word_view_android.dart
+++ frontend/snapstory/lib/views/home/find_word_view_android.dart
@@ -1,98 +1,9 @@
-// import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
-// import 'package:flutter/material.dart';
-// import 'package:vector_math/vector_math_64.dart';
-//
-// class ARViewAndroid extends StatefulWidget {
-//   const ARViewAndroid({Key? key}) : super(key: key);
-//   @override
-//   _ARViewAndroidState createState() => _ARViewAndroidState();
-// }
-//
-// class _ARViewAndroidState extends State<ARViewAndroid> {
-//   late ArCoreController arCoreController;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Hello World'),
-//         ),
-//         body: ArCoreView(
-//           onArCoreViewCreated: _onArCoreViewCreated,
-//         ),
-//       ),
-//     );
-//   }
-//
-//   void _onArCoreViewCreated(ArCoreController controller) {
-//     arCoreController = controller;
-//
-//     _addSphere(arCoreController);
-//     _addCylindre(arCoreController);
-//     _addCube(arCoreController);
-//   }
-//
-//   void _addSphere(ArCoreController controller) {
-//     final material = ArCoreMaterial(
-//         color: Color.fromARGB(120, 66, 134, 244));
-//     final sphere = ArCoreSphere(
-//       materials: [material],
-//       radius: 0.1,
-//     );
-//     final node = ArCoreNode(
-//       shape: sphere,
-//       position: Vector3(0, 0, -1.5),
-//     );
-//     controller.addArCoreNode(node);
-//   }
-//
-//   void _addCylindre(ArCoreController controller) {
-//     final material = ArCoreMaterial(
-//       color: const Color.fromRGBO(10, 10, 10, 10),
-//       reflectance: 1.0,
-//     );
-//     final cylindre = ArCoreCylinder(
-//       materials: [material],
-//       radius: 0.5,
-//       height: 0.3,
-//     );
-//     final node = ArCoreNode(
-//       shape: cylindre,
-//       position: Vector3(0.0, -0.5, -2.0),
-//     );
-//     controller.addArCoreNode(node);
-//   }
-//
-//   void _addCube(ArCoreController controller) {
-//     final material = ArCoreMaterial(
-//       color: Color.fromARGB(120, 66, 134, 244),
-//       metallic: 1.0,
-//     );
-//     final cube = ArCoreCube(
-//       materials: [material],
-//       size: Vector3(0.5, 0.5, 0.5),
-//     );
-//     final node = ArCoreNode(
-//       shape: cube,
-//       position: Vector3(-0.5, 0.5, -3.5),
-//     );
-//     controller.addArCoreNode(node);
-//   }
-//
-//   @override
-//   void dispose() {
-//     arCoreController.dispose();
-//     super.dispose();
-//   }
-// }
-
 import 'dart:io';
 
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
-import 'package:camera/camera.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:native_screenshot/native_screenshot.dart';
 import 'package:vector_math/vector_math_64.dart';
 
 class ARViewAndroid extends StatefulWidget {
@@ -104,6 +15,7 @@ class ARViewAndroid extends StatefulWidget {
 
 class _ARViewAndroidState extends State<ARViewAndroid> {
   late ArCoreController arCoreController;
+  late bool checked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -119,74 +31,53 @@ class _ARViewAndroidState extends State<ARViewAndroid> {
       ),
       body: Stack(
         children: [
-          ArCoreView(onArCoreViewCreated: _onArCoreViewCreated),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.15,
-            left: MediaQuery.of(context).size.width * 0.1,
-            child: DottedBorder(
-              color: const Color.fromARGB(255, 0, 0, 0),
-              //color of dotted/dash line
-              borderType: BorderType.RRect,
-              radius: const Radius.circular(30),
-              strokeWidth: 2,
-              //thickness of dash/dots
-              dashPattern: [10, 6],
-              //dash patterns, 10 is dash width, 6 is space width
-              child: Container(
-                  //inner container
-                  height: MediaQuery.of(context).size.height *
-                      0.6, //height of inner container
-                  width: MediaQuery.of(context).size.width *
-                      0.8, //width to 100% match to parent container.
-                  color: const Color.fromRGBO(
-                      0, 0, 0, 0) //background color of inner container
-                  ),
-            ),
+          ArCoreView(
+            onArCoreViewCreated: _onArCoreViewCreated,
+            enableTapRecognizer: true,
           ),
+          if (!checked)
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.15,
+              left: MediaQuery.of(context).size.width * 0.1,
+              child: IgnorePointer(
+                ignoring: true,
+                child: DottedBorder(
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  //color of dotted/dash line
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(30),
+                  strokeWidth: 2,
+                  //thickness of dash/dots
+                  dashPattern: [10, 6],
+                  //dash patterns, 10 is dash width, 6 is space width
+                  child: Container(
+                      //inner container
+                      height: MediaQuery.of(context).size.height *
+                          0.6, //height of inner container
+                      width: MediaQuery.of(context).size.width *
+                          0.8, //width to 100% match to parent container.
+                      color: const Color.fromRGBO(
+                          0, 0, 0, 0) //background color of inner container
+                      ),
+                ),
+              ),
+            ),
+          if (checked) const Text('TAP'),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.camera_alt), // Provide an onPressed callback.
         onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Obtain a list of the available cameras on the device.
-            final cameras = await availableCameras();
-
-            // Get a specific camera from the list of available cameras.
-            final CameraDescription camera = cameras.first;
-
-            late CameraController _controller = CameraController(
-              // Get a specific camera from the list of available cameras.
-              camera, // Define the resolution to use.
-              ResolutionPreset.max,
-            );
-            // Ensure that the camera is initialized.
-            await _controller.initialize();
-
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
-            final image = await _controller.takePicture();
-
-            if (!mounted) return;
-
-            // If the picture was taken, display it on a new screen.
-            await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
-                  // Pass the automatically generated path to
-                  // the DisplayPictureScreen widget.
-                  imagePath: image.path,
-                ),
+          String? path = await NativeScreenshot.takeScreenshot();
+          await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => DisplayPictureScreen(
+                // Pass the automatically generated path to
+                // the DisplayPictureScreen widget.
+                imagePath: path!,
               ),
-            );
-
-            _controller.dispose();
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e.toString());
-          }
+            ),
+          );
         },
       ),
     );
@@ -194,6 +85,7 @@ class _ARViewAndroidState extends State<ARViewAndroid> {
 
   void _onArCoreViewCreated(ArCoreController controller) {
     arCoreController = controller;
+    arCoreController?.onNodeTap = (name) => onTapHandler(name);
 
     _addSphere(arCoreController);
     _addCylindre(arCoreController);
@@ -207,6 +99,7 @@ class _ARViewAndroidState extends State<ARViewAndroid> {
       radius: 0.1,
     );
     final node = ArCoreNode(
+      name: 'sphere',
       shape: sphere,
       position: Vector3(0, 0, -1.5),
     );
@@ -224,6 +117,7 @@ class _ARViewAndroidState extends State<ARViewAndroid> {
       height: 0.3,
     );
     final node = ArCoreNode(
+      name: 'cylindre',
       shape: cylindre,
       position: Vector3(0.0, -0.5, -2.0),
     );
@@ -240,6 +134,24 @@ class _ARViewAndroidState extends State<ARViewAndroid> {
       size: Vector3(0.5, 0.5, 0.5),
     );
     final node = ArCoreNode(
+      name: 'cube',
+      shape: cube,
+      position: Vector3(-0.5, 0.5, -3.5),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addText(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Color.fromARGB(120, 66, 134, 244),
+      metallic: 1.0,
+    );
+    final cube = ArCoreCube(
+      materials: [material],
+      size: Vector3(0.5, 0.5, 0.5),
+    );
+    final node = ArCoreNode(
+      name: 'cube',
       shape: cube,
       position: Vector3(-0.5, 0.5, -3.5),
     );
@@ -250,6 +162,16 @@ class _ARViewAndroidState extends State<ARViewAndroid> {
   void dispose() {
     arCoreController.dispose();
     super.dispose();
+  }
+
+  void onTapHandler(String name) {
+    checked == true ? checked = false : checked = true;
+    // showDialog<void>(
+    //   context: context,
+    //   builder: (BuildContext context) =>
+    //       AlertDialog(content: Text('onNodeTap on $name')),
+    // );
+    setState(() {});
   }
 }
 
