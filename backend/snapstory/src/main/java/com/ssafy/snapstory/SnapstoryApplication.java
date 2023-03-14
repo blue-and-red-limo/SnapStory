3cf4095d9ac50e4644a417ef44fc9b4f42e8871c
Myungho-Kim <myungho96@naver.com>
2023-03-14 09:12:35 +0900

    Fix: fix swagger
     fix swagger

--- backend/snapstory/src/main/java/com/ssafy/snapstory/SnapstoryApplication.java
+++ backend/snapstory/src/main/java/com/ssafy/snapstory/SnapstoryApplication.java
@@ -2,15 +2,7 @@ package com.ssafy.snapstory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-import java.net.URI;
-
-import static org.springframework.web.servlet.function.RequestPredicates.GET;
-import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 
 @SpringBootApplication
@@ -19,9 +11,5 @@ public class SnapstoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SnapstoryApplication.class, args);
 	}
-	@Bean
-	RouterFunction<ServerResponse> routerFunction() {
-		return route(GET("/api/v1/swagger"), req ->
-				ServerResponse.temporaryRedirect(URI.create("/swagger-ui/index.html")).build());
-	}
+
 }
