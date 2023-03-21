ee1cc4ad355d8fa12aca5030f6a55a02f2ef520f
eunbicho <puppy101976@naver.com>
2023-03-21 15:59:03 +0900

    Feat: change to ar flutter plugin

--- frontend/snapstory/lib/services/ar_ai_service.dart
+++ frontend/snapstory/lib/services/ar_ai_service.dart
@@ -10,7 +10,7 @@ class ARAIService {
 
   Future<String> postPictureAndGetWord({required String path}) async {
     var request =
-        http.MultipartRequest('POST', Uri.parse('$aiBase/predictions/drawings'))
+        http.MultipartRequest('POST', Uri.parse('$aiBase/predictions/objects'))
           ..files.add(await http.MultipartFile.fromPath('file', path));
     var response = await request.send();
     if (response.statusCode == 200) {
