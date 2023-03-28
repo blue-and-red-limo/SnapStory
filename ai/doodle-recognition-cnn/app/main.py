8fa4a485e4da8b6ba1be06bf7f1b9c2d5a9f25f0
kdoubleh22 <kdoubleh22@naver.com>
2023-03-28 15:13:03 +0900

    Feat: load model success
    Former-commit-id: 26a5b4aaae23c357cfded2740aa4183f9eb79070

--- ai/doodle-recognition-cnn/app/main.py
+++ ai/doodle-recognition-cnn/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 # 'base64_to_numpy' 함수에 사용.
+import base64
 from io import BytesIO
 from PIL import Image
 import numpy as np
@@ -11,6 +12,13 @@ import os
 from tensorflow.python.keras import layers
 from tensorflow import keras
 import tensorflow as tf
+
+f= open(os.getcwd()+"/app/model/mini_classes.txt","r")
+# And for reading use
+classes = f.readlines()
+f.close()
+
+_classes = [c.replace('\n','').replace(' ','_') for c in classes]
 
 app = FastAPI()
 
@@ -29,10 +37,24 @@ async def predict(request: Base64Request):
     print("before load_model()")
     model = load_model()
     print("after load_model()")
-    
-    print(model)
 
-    return "connection success"
+    image_numpy = base64_to_numpy(request.base64_file)
+    image_numpy2 = image_numpy/255.0
+
+    
+    pred=model.predict(image_numpy)[0]
+    print(pred)
+    ind = (-pred).argsort()[:5]
+    latex = [_classes[x]for x in ind]
+    print(latex)
+
+    pred2=model.predict(image_numpy2)[0]
+    print(pred2)
+    ind = (-pred2).argsort()[:5]
+    latex2 = [_classes[x]for x in ind]
+    print(latex2)
+
+    return latex
 
 def load_model():
     # Define the model architecture on the server side
@@ -54,7 +76,7 @@ def load_model():
 
     # Load model weights
     model.load_weights(CNNDIR+'keras_weights.h5')
-    
+
     return model
 
 # base64 encoded image file을 gray-scale (1,28,28,1) shape np array로.
