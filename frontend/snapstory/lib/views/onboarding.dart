b7c193cbbd79c096718670db93e83fb55bead6c1
eunbicho <puppy101976@naver.com>
2023-04-04 17:55:51 +0900

    Fix: modify onboarding tutorial

--- frontend/snapstory/lib/views/onboarding.dart
+++ frontend/snapstory/lib/views/onboarding.dart
@@ -12,30 +12,33 @@ class OnBoardingPage extends StatelessWidget {
       pages: [
         PageViewModel(
             title: '',
-            body: '안녕! 난 스내피라고 해.',
+            body: '안녕! 난 Snappy라고 해.\n'
+                'Sna!p story에 온걸 환영해!',
             image: Image.asset('assets/tuto/(1)intro.gif'),
             decoration: getPageDecoration(),
         ),
         PageViewModel(
             title: '',
-            body: 'tutorial 2',
+            body: '영어 단어로 알고 싶은 사물이 있다고?\n사진을 찍어서 나에게 물어봐!',
             image: Image.asset('assets/tuto/(2)aitale.gif'),
             decoration: getPageDecoration()
         ), PageViewModel(
             title: '',
-            body: 'tutorial 3',
+            body: '찾은 단어로 나만의 동화책을 만들 수 있어.\n'
+                '매일 새로운 동화가 가득해!',
             image: Image.asset('assets/tuto/(3)aitale.gif'),
             decoration: getPageDecoration()
         ),
         PageViewModel(
             title: '',
-            body: 'tutorial 4',
+            body: '재밌는 그림 퀴즈도 준비했어.\n'
+                '다 그리면 재밌는 동화가 나와!',
             image: Image.asset('assets/tuto/(4)quiztale.gif'),
             decoration: getPageDecoration()
         ),
         PageViewModel(
             title: '',
-            body: 'tutorial 5',
+            body: '찾은 단어들은 단어장에서 다시 볼 수 있어! 나만의 동화책들은 도서관에서 볼 수 있지.\n\n자, 이제 동화를 만들러 가볼까?\n',
             image: Image.asset('assets/tuto/(5)my.gif'),
             decoration: getPageDecoration()
         ),
@@ -69,21 +72,21 @@ class OnBoardingPage extends StatelessWidget {
   PageDecoration getPageDecoration() {
     return const PageDecoration(
         titleTextStyle: TextStyle(
-            fontSize: 1,
+            fontSize: 0,
             // fontWeight: FontWeight.bold
         ),
         bodyTextStyle: TextStyle(
             fontSize: 20,
-            color: Colors.blue
+            color: Colors.green
         ),
         // imagePadding: EdgeInsets.only(top: 40),
         bodyAlignment: Alignment.topCenter,
         titlePadding: EdgeInsets.zero,
         bodyFlex: 5,
-        imageAlignment: Alignment.center,
+        imageAlignment: Alignment.bottomCenter,
       imagePadding: EdgeInsets.zero,
       bodyPadding: EdgeInsets.zero,
-      imageFlex: 30, // 이미지 위치 조정
+      imageFlex: 15, // 이미지 위치 조정
     );
   }
 }
