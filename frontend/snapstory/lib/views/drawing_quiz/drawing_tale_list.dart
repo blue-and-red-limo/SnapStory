6b02b9026131f43cf0ce0d3bc9a1cd7624463103
YJW <ykm9397@naver.com>
2023-04-05 09:25:02 +0900

    FE/Fix: fix bottom bar in quiz-tale-view

--- frontend/snapstory/lib/views/drawing_quiz/drawing_tale_list.dart
+++ frontend/snapstory/lib/views/drawing_quiz/drawing_tale_list.dart
@@ -13,101 +13,100 @@ class DrawingTaleListState extends State<DrawingTaleList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Stack(
-        children: [
-          Container(
-            padding: EdgeInsets.only(
-                bottom: MediaQuery.of(context).size.height * 0.15,
-                top: MediaQuery.of(context).size.width * 0.4),
-            height: MediaQuery.of(context).size.height,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('assets/main/bg-main2_2.png'),
+        body: SafeArea(
+      child: Center(
+        child: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).size.height * 0.11,
+                  top: MediaQuery.of(context).size.height * 0.12),
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/main/bg-main2_2.png'),
+                ),
+              ),
+              child: ListView(
+                padding: EdgeInsets.only(top: 0),
+                children: [
+                  // 신데렐라
+                  IconButton(
+                    padding: EdgeInsets.all(
+                        MediaQuery.of(context).size.height * 0.01),
+                    iconSize: MediaQuery.of(context).size.height * 0.2,
+                    icon: Image.asset(
+                        "assets/quizTaleList/btn-quiz-cinderella.png"),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DrawingView(1)));
+                    },
+                  ),
+                  // 백설공주
+                  IconButton(
+                    padding: EdgeInsets.all(
+                        MediaQuery.of(context).size.height * 0.01),
+                    iconSize: MediaQuery.of(context).size.height * 0.2,
+                    icon: Image.asset(
+                        "assets/quizTaleList/btn-quiz-snowwhite.png"),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DrawingView(2)));
+                    },
+                  ),
+                  // 잠자는 숲속의 공주
+                  IconButton(
+                    padding: EdgeInsets.all(
+                        MediaQuery.of(context).size.height * 0.01),
+                    iconSize: MediaQuery.of(context).size.height * 0.2,
+                    icon: Image.asset(
+                        "assets/quizTaleList/btn-quiz-sleepingbeauty.png"),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DrawingView(3)));
+                    },
+                  ),
+                  // 라푼젤
+                  IconButton(
+                    padding: EdgeInsets.all(
+                        MediaQuery.of(context).size.height * 0.01),
+                    iconSize: MediaQuery.of(context).size.height * 0.2,
+                    icon: Image.asset(
+                        "assets/quizTaleList/btn-quiz-rapunzel.png"),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DrawingView(4)));
+                    },
+                  ),
+                  // 미녀와 야수
+                  IconButton(
+                    padding: EdgeInsets.all(
+                        MediaQuery.of(context).size.height * 0.01),
+                    iconSize: MediaQuery.of(context).size.height * 0.2,
+                    icon: Image.asset(
+                        "assets/quizTaleList/btn-quiz-beautyandbeast.png"),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DrawingView(5)));
+                    },
+                  ),
+                ],
               ),
             ),
-            child: ListView(
-              padding: EdgeInsets.only(top: 0),
-              children: [
-                // 신데렐라
-                IconButton(
-                  padding:
-                      EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
-                  iconSize: MediaQuery.of(context).size.height * 0.2,
-                  icon: Image.asset(
-                      "assets/quizTaleList/btn-quiz-cinderella.png"),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DrawingView(1)));
-                  },
-                ),
-                // 백설공주
-                IconButton(
-                  padding:
-                      EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
-                  iconSize: MediaQuery.of(context).size.height * 0.2,
-                  icon:
-                      Image.asset("assets/quizTaleList/btn-quiz-snowwhite.png"),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DrawingView(2)));
-                  },
-                ),
-                // 잠자는 숲속의 공주
-                IconButton(
-                  padding:
-                      EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
-                  iconSize: MediaQuery.of(context).size.height * 0.2,
-                  icon: Image.asset(
-                      "assets/quizTaleList/btn-quiz-sleepingbeauty.png"),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DrawingView(3)));
-                  },
-                ),
-                // 라푼젤
-                IconButton(
-                  padding:
-                      EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
-                  iconSize: MediaQuery.of(context).size.height * 0.2,
-                  icon:
-                      Image.asset("assets/quizTaleList/btn-quiz-rapunzel.png"),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DrawingView(4)));
-                  },
-                ),
-                // 미녀와 야수
-                IconButton(
-                  padding:
-                      EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
-                  iconSize: MediaQuery.of(context).size.height * 0.2,
-                  icon: Image.asset(
-                      "assets/quizTaleList/btn-quiz-beautyandbeast.png"),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DrawingView(5)));
-                  },
-                ),
-              ],
-            ),
-          ),
 
-          // 상단 버튼
-          Padding(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            child: Align(
+            // 상단 버튼
+            Align(
               alignment: Alignment.topCenter,
               child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -131,26 +130,26 @@ class DrawingTaleListState extends State<DrawingTaleList> {
                         )),
                   ]),
             ),
-          ),
 
-          // 하단 바
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.08,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(32),
-                    topRight: Radius.circular(32)),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/main/bg-bar.png'),
+            // 하단 바
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.08,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(32),
+                      topRight: Radius.circular(32)),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/main/bg-bar.png'),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     ));
   }
